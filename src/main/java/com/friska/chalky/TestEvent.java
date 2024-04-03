06d208d35619213fd71e71d4b5d7432b68bbe34c
LaFriska <harold.gao205@gmail.com>
2024-04-03 16:25:18 +1100

    Established sql draft

--- src/main/java/com/friska/chalky/TestEvent.java
+++ src/main/java/com/friska/chalky/TestEvent.java
@@ -2,7 +2,6 @@ package com.friska.chalky;
 
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
