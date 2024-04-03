2d12c4e247fa63a2d4f432435eb99e53d6c00be3
LaFriska <harold.gao205@gmail.com>
2024-04-03 14:20:23 +1100

    Initial Commit

---
+++ src/main/java/com/friska/chalky/Main.java
@@ -0,0 +1,28 @@
+package com.friska.chalky;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        JDABuilder builder = JDABuilder.createDefault(fetchToken());
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+        builder.addEventListeners(new TestEvent());
+        builder.build();
+    }
+
+    private static String fetchToken(){
+        try {
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("config.properties"));
+            return prop.getProperty("token");
+        }catch (IOException ignored){
+            throw new RuntimeException("CANNOT ACCESS TOKEN");
+        }
+    }
+}
---
+++ src/main/java/com/friska/chalky/TestEvent.java
@@ -0,0 +1,26 @@
+package com.friska.chalky;
+
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class TestEvent extends ListenerAdapter {
+
+    private static String testString = "[MATH4204](https://programsandcourses.anu.edu.au/2024/course/MATH4204) Algebraic Topology Honours\n" +
+            "[MATH4204](https://programsandcourses.anu.edu.au/2024/course/MATH4204) Algebraic Topology Honours\n" +
+            "[MATH4204](https://programsandcourses.anu.edu.au/2024/course/MATH4204) Algebraic Topology Honours\n" +
+            "[MATH4204](https://programsandcourses.anu.edu.au/2024/course/MATH4204) Algebraic Topology Honours";
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if(event.getMessage().getContentRaw().equals("test")){
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle("Degree Plan");
+            eb.addField("Semester 1", testString, true);
+            eb.addField("Semester 2", testString, true);
+            event.getChannel().sendMessageEmbeds(eb.build()).queue();
+        }
+    }
+}
