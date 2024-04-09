4a2af880be8a902e059827c5cd516eeee8d415d5
Harold Gao <harold.gao205@gmail.com>
2024-04-10 00:00:21 +1000

    Reformat files, and fixed hyperlink bug

--- course_processor/wrangler.py
+++ course_processor/wrangler.py
@@ -4,6 +4,8 @@ import re
 
 def start(unnest):
     sort(unnest)
+    reformat()
+    hyperlink_requisites()
 
 
 # Bubble sort algorithm to sort the courses json, and disgard unnecessary nesting of objects. Input false into this method
@@ -18,7 +20,8 @@ def sort(unnest):
         courses = courses["Items"]
 
     if len(courses) != len(reqs):
-        print(f"Serious issue! There are {len(courses)} courses, but there are {len(reqs)} requisites, which is a mismatch. Consider re-running the scraper.")
+        print(
+            f"Serious issue! There are {len(courses)} courses, but there are {len(reqs)} requisites, which is a mismatch. Consider re-running the scraper.")
 
     for i in range(1, len(courses)):
         for j in range(len(courses) - i):
@@ -41,6 +44,31 @@ def sort(unnest):
     print('Sort finished')
 
 
+# Things like remove unnecessary line breaks, removing awkward spaces before periods etc.
+def reformat():
+    print('Removing unnecessary linebreaks')
+    reqs = json.load(open('requisites.json'))
+    courses = json.load(open('courses.json'))
+    for i in range(len(reqs)):
+        reqs[i]["req"] = reformat_reqs(reqs[i]["req"], courses)
+    print('Writing to file')
+    f = open('requisites.json', 'w')
+    f.write(json.dumps(reqs, indent=4))
+    print('Linebreaks removed')
+
+
+
+def reformat_reqs(requisite, courses):
+    words = re.split(r'\n', requisite)
+    for i in range(len(words)):
+        if has_course(words[i], courses):
+            requisite = requisite.replace(f"\n{words[i]}\n", f" {words[i]} ")
+    requisite = requisite.replace(" .", ".")
+    requisite = requisite.replace(" ,", ",")
+    requisite = requisite.replace(" ;", ";")
+    return requisite
+
+
 #Scans through entire requisites json and replace any words that are an ANU course with a hyperlinked version.
 def hyperlink_requisites():
     print('Hyperlinking course codes in requisites')
@@ -58,14 +86,18 @@ def hyperlink_requisites():
 # Hyperlinks one requisite string
 def hyperlink_requisite(requisite, courses):
     words = re.split(r'\s+', requisite)
+    linked_words = []
     for i in range(len(words)):
-        if has_course(words[i], courses):
-            requisite = re.sub(r'\b%s\b' % re.escape(words[i]), get_hyperlinked_course(words[i]), requisite)
+        if has_course(words[i], courses) and words[i] not in linked_words:
+            requisite = requisite.replace(words[i], get_hyperlinked_course(words[i]))
+            linked_words.append(words[i])
     return requisite
 
 
 # Performs a binary search on courses and returns if word is in courses.
 def has_course(word, courses):
+    if len(word) > 12:
+        return False
     l = 0
     r = len(courses) - 1
     m = r // 2
@@ -88,4 +120,8 @@ def get_hyperlinked_course(courseCode):
     return f"[{courseCode}](https://programsandcourses.anu.edu.au/2024/course/{courseCode})"
 
 
-start(True)
+def replace(s, keyword, replacement):
+    return re.sub(r'\b%s\b' % re.escape(keyword), replacement, s)
+
+
+start(False)
